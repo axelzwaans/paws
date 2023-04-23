b1316a39e64ca30a911ac63a1ba01241ea5bb26c
Axel Zwaans <axelzwaans@hotmail.com>
2023-04-23 08:50:55 +0000

    Add signin and signup forms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom"
+import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+          <Switch>
+            <Route path="/signup" component={SignUpForm} />
+            <Route path="/signin" component={SignInForm} />
+            <Route render={() => <h1>Page not found!</h1>} />
+          </Switch>
+      </Container>
     </div>
   );
 }
