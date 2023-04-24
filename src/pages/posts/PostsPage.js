90f6b5f87e0b2b8d385f64675e784a439c739468
Axel Zwaans <axelzwaans@hotmail.com>
2023-04-24 18:41:21 +0000

    Create PostEditForm page

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -15,6 +15,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -72,7 +73,7 @@ function PostsPage({ message, filter = "" }) {
               dataLength={posts.results.length}
               loader={<Asset spinner />}
               hasMore={!!posts.next}
-              next={() => {}}
+              next={() => fetchMoreData(posts, setPosts)}
               />
             ) : (
               <Container className={appStyles.Content}>
