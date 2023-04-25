a51876b79f1016c3144531b538a02d8c80b82ec6
Axel Zwaans <axelzwaans@hotmail.com>
2023-04-25 17:12:38 +0000

    Create profile page header

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -10,8 +10,12 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
 
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostPage() {
   const { id } = useParams();
@@ -56,9 +60,20 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
